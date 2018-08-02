09ea990cf89df8596c768eb21c479244e8b1a9a6
takeshing <41040717+takeshing@users.noreply.github.com>
2018-08-03 00:50:53 +0900

    タテヨコレイアウト 変更

--- takeshing/FRViewController.swift
+++ takeshing/FRViewController.swift
@@ -24,8 +24,17 @@ class FRViewController : UIViewController{
     let tab: UITabBarController = UITabBarController()
     var audiobgm: AVAudioPlayer!
     var audio: AVAudioPlayer!
+    var portraitConstaints:[NSLayoutConstraint] = []
+    var landscapeConstaints:[NSLayoutConstraint] = []
+    var status_height : CGFloat = 0.0
+    var tabbar_hieght : CGFloat = 0.0
+    var p_height : CGFloat = 0.0
+    var s_height : CGFloat = 0.0
+    var p_width : CGFloat = 0.0
+    var s_width : CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let bgm = Bundle.main.path(forResource: "bgm", ofType: "mp3")
         do {
             audiobgm = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: bgm!))
@@ -133,56 +142,8 @@ class FRViewController : UIViewController{
         viewBase.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
         viewBase.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
 
-        viewP.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0).isActive = true
-        viewP.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: 0).isActive = true
-        viewP.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-        viewP.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.6).isActive = true
+        reloade()
         
-        viewS.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0).isActive = true
-        viewS.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: 0).isActive = true
-        viewS.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-        viewS.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.4).isActive = true
-        viewS.topAnchor.constraint(equalTo:viewP.bottomAnchor, constant: 0).isActive = true
-        
-        btn1.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0).isActive = true
-        btn1.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0).isActive = true
-        btn1.widthAnchor.constraint(equalToConstant: self.view.frame.width / 4).isActive = true
-        btn1.heightAnchor.constraint(equalToConstant: (self.view.frame.height * 0.4 - tab.tabBar.frame.size.height) / 2).isActive = true
-        
-        btn2.leadingAnchor.constraint(equalTo:btn1.trailingAnchor, constant: 0).isActive = true
-        btn2.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0).isActive = true
-        btn2.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn2.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
-
-        btn3.leadingAnchor.constraint(equalTo:btn2.trailingAnchor, constant: 0).isActive = true
-        btn3.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0).isActive = true
-        btn3.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn3.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
-
-        btn4.leadingAnchor.constraint(equalTo:btn3.trailingAnchor, constant: 0).isActive = true
-        btn4.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0).isActive = true
-        btn4.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn4.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
-
-        btn5.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0).isActive = true
-        btn5.topAnchor.constraint(equalTo:btn1.bottomAnchor, constant: 0).isActive = true
-        btn5.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn5.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
-
-        btn6.leadingAnchor.constraint(equalTo:btn5.trailingAnchor, constant: 0).isActive = true
-        btn6.topAnchor.constraint(equalTo:btn2.bottomAnchor, constant: 0).isActive = true
-        btn6.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn6.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
-
-        btn7.leadingAnchor.constraint(equalTo:btn6.trailingAnchor, constant: 0).isActive = true
-        btn7.topAnchor.constraint(equalTo:btn3.bottomAnchor, constant: 0).isActive = true
-        btn7.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn7.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
-
-        btn8.leadingAnchor.constraint(equalTo:btn7.trailingAnchor, constant: 0).isActive = true
-        btn8.topAnchor.constraint(equalTo:btn4.bottomAnchor, constant: 0).isActive = true
-        btn8.widthAnchor.constraint(equalTo: btn1.widthAnchor).isActive = true
-        btn8.heightAnchor.constraint(equalTo: btn1.heightAnchor).isActive = true
     }
     @objc func tapped(sender : AnyObject) {
         let b : UIButton = sender as! UIButton
@@ -191,5 +152,129 @@ class FRViewController : UIViewController{
         print(b.image(for: UIControlState.normal))
         
     }
+    override func viewDidAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.rotationChange(notification:)), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    @objc
+    func rotationChange(notification: NSNotification){
+        reloade()
+    }
+    func reloade(){
+        status_height = UIApplication.shared.statusBarFrame.size.height
+        tabbar_hieght = UITabBarController().tabBar.frame.size.height
+        print(status_height)
+        print(tabbar_hieght)
+        if (self.view.frame.height > self.view.frame.width){
+            p_height = (self.view.frame.height - status_height - tabbar_hieght) * 0.65
+            s_height = (self.view.frame.height - status_height - tabbar_hieght) * 0.35
+            p_width = self.view.frame.width
+            s_width = self.view.frame.width
+            portraitConstaints = [
+                viewP.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0),
+                viewP.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: 0),
+                viewP.topAnchor.constraint(equalTo:self.view.topAnchor, constant:status_height),
+                viewP.bottomAnchor.constraint(equalTo:self.view.bottomAnchor, constant: -(s_height + tabbar_hieght)),
+                viewS.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0),
+                viewS.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: 0),
+                viewS.topAnchor.constraint(equalTo:self.view.topAnchor, constant:status_height + p_height),
+                viewS.bottomAnchor.constraint(equalTo:self.view.bottomAnchor, constant: -tabbar_hieght),
+                btn1.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0),
+                btn1.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0),
+                btn1.widthAnchor.constraint(equalToConstant: self.view.frame.width / 4),
+                btn1.heightAnchor.constraint(equalToConstant: (self.view.frame.height * 0.4 - tab.tabBar.frame.size.height) / 2),
+                btn2.leadingAnchor.constraint(equalTo:btn1.trailingAnchor, constant: 0),
+                btn2.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0),
+                btn2.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn2.heightAnchor.constraint(equalTo: btn1.heightAnchor),
+                btn3.leadingAnchor.constraint(equalTo:btn2.trailingAnchor, constant: 0),
+                btn3.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0),
+                btn3.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn3.heightAnchor.constraint(equalTo: btn1.heightAnchor),
+                btn4.leadingAnchor.constraint(equalTo:btn3.trailingAnchor, constant: 0),
+                btn4.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0),
+                btn4.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn4.heightAnchor.constraint(equalTo: btn1.heightAnchor),
+                btn5.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0),
+                btn5.topAnchor.constraint(equalTo:btn1.bottomAnchor, constant: 0),
+                btn5.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn5.heightAnchor.constraint(equalTo: btn1.heightAnchor),
+                btn6.leadingAnchor.constraint(equalTo:btn5.trailingAnchor, constant: 0),
+                btn6.topAnchor.constraint(equalTo:btn2.bottomAnchor, constant: 0),
+                btn6.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn6.heightAnchor.constraint(equalTo: btn1.heightAnchor),
+                btn7.leadingAnchor.constraint(equalTo:btn6.trailingAnchor, constant: 0),
+                btn7.topAnchor.constraint(equalTo:btn3.bottomAnchor, constant: 0),
+                btn7.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn7.heightAnchor.constraint(equalTo: btn1.heightAnchor),
+                btn8.leadingAnchor.constraint(equalTo:btn7.trailingAnchor, constant: 0),
+                btn8.topAnchor.constraint(equalTo:btn4.bottomAnchor, constant: 0),
+                btn8.widthAnchor.constraint(equalTo: btn1.widthAnchor),
+                btn8.heightAnchor.constraint(equalTo: btn1.heightAnchor)
+            ]
+        } else {
+            p_height = self.view.frame.height - status_height - tabbar_hieght
+            s_height = self.view.frame.height - status_height - tabbar_hieght
+            p_width = self.view.frame.width * 0.7
+            s_width = self.view.frame.width * 0.3
+            landscapeConstaints = [
+                viewP.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: 0),
+                viewP.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: -s_width),
+                viewP.topAnchor.constraint(equalTo:self.view.topAnchor, constant:0),
+                viewP.bottomAnchor.constraint(equalTo:self.view.bottomAnchor, constant: -tabbar_hieght),
+                viewS.leadingAnchor.constraint(equalTo:self.view.leadingAnchor, constant: p_width),
+                viewS.trailingAnchor.constraint(equalTo:self.view.trailingAnchor, constant: 0),
+                viewS.topAnchor.constraint(equalTo:self.view.topAnchor, constant:status_height),
+                viewS.bottomAnchor.constraint(equalTo:self.view.bottomAnchor, constant: -tabbar_hieght),
+                btn1.leadingAnchor.constraint(equalTo:viewS.leadingAnchor, constant: 0),
+                btn1.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0),
+                btn1.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn1.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn2.leadingAnchor.constraint(equalTo:btn1.trailingAnchor, constant: 0),
+                btn2.topAnchor.constraint(equalTo:viewS.topAnchor, constant: 0),
+                btn2.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn2.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn3.leadingAnchor.constraint(equalTo:viewS.leadingAnchor, constant: 0),
+                btn3.topAnchor.constraint(equalTo:btn1.bottomAnchor, constant: 0),
+                btn3.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn3.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn4.leadingAnchor.constraint(equalTo:btn3.trailingAnchor, constant: 0),
+                btn4.topAnchor.constraint(equalTo:btn2.bottomAnchor, constant: 0),
+                btn4.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn4.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn5.leadingAnchor.constraint(equalTo:viewS.leadingAnchor, constant: 0),
+                btn5.topAnchor.constraint(equalTo:btn3.bottomAnchor, constant: 0),
+                btn5.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn5.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn6.leadingAnchor.constraint(equalTo:btn5.trailingAnchor, constant: 0),
+                btn6.topAnchor.constraint(equalTo:btn4.bottomAnchor, constant: 0),
+                btn6.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn6.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn7.leadingAnchor.constraint(equalTo:viewS.leadingAnchor, constant: 0),
+                btn7.topAnchor.constraint(equalTo:btn5.bottomAnchor, constant: 0),
+                btn7.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn7.heightAnchor.constraint(equalToConstant: s_height / 4),
+                btn8.leadingAnchor.constraint(equalTo:btn7.trailingAnchor, constant: 0),
+                btn8.topAnchor.constraint(equalTo:btn6.bottomAnchor, constant: 0),
+                btn8.widthAnchor.constraint(equalToConstant: s_width / 2),
+                btn8.heightAnchor.constraint(equalToConstant: s_height / 4),
+            ]
+        }
 
+        if (self.view.frame.height > self.view.frame.width){
+            portraitConstaints.forEach { (cons) in
+                cons.isActive = true
+            }
+            landscapeConstaints.forEach { (cons) in
+                cons.isActive = false
+            }
+        } else {
+            portraitConstaints.forEach { (cons) in
+                cons.isActive = false
+            }
+            landscapeConstaints.forEach { (cons) in
+                cons.isActive = true
+            }
+        }
+    }
 }
